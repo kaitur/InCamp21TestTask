b97bc2c28de6f2223942d92af89d7bb15707dedd
Artur Kaidanskyi <artur.kaidanskyi@gmail.com>
2021-06-28 18:30:24 +0300

    creating an empty console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InCamp21TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
